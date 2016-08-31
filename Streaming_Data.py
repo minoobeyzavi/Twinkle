bb0dba03ea141e65345567d2a791437bb01e31b3
Minoo <minoo.beyzavi@gmail.com>
2016-08-30 21:01:19 -0700

    Correction to the previous commit
    This commit uploads the file mentioned in the previous commit.

---
+++ Streaming_Data.py
@@ -0,0 +1,32 @@
+# Running this code receives the current stream of tweets from the Twitter
+# Streaming API and saves it to a local MongoDB database.
+
+try:
+    import json
+except ImportError:
+    import simplejson as json
+
+from twitter import Twitter, OAuth, TwitterHTTPError, TwitterStream
+import pymongo
+from pymongo import MongoClient
+
+ACCESS_TOKEN = '716396311650500609-xlnuQgwmfEwFqMk95yIUUP7DpI0A3QV'
+ACCESS_SECRET = '6XcRuU520RWH14vrRh8sUI98UCiPJ25q1Cr50Kmx30Q8F'
+# Add unique customer key bellow after registering with the twitter API.
+CONSUMER_KEY = ''
+CONSUMER_SECRET = 'A49FPaTpKxdmboMrsoXzIKqZfWFvJOJCns1ifRrLe4oVkJDPmK'
+oauth = OAuth(ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
+twitter_stream = TwitterStream(auth=oauth)
+iterator = twitter_stream.statuses.sample(language="en")
+
+client = MongoClient()
+db = client['tweets_db']
+
+# Choose number of tweets wisely depending on available space.
+tweet_count = 1000000
+
+for tweet in iterator:
+    tweet_count -= 1
+    db.tweets.insert_one(tweet)
+    if tweet_count <= 0:
+        break
